dcb9a846dc121c96e2d59fae0b3ee7cfb2eaf380
frtavonatti <frtavonatti@gmail.com>
2024-05-07 16:42:55 +0100

    Part 3-c: Ejercicio 3.14 finalizado
    POST funcional
    DELETE pendiente

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 const Person = require('./model/person')
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
+// app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
